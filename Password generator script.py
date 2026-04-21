816380a90ad5c3dfb7c792a5a7c07bc7db3f1aba
themanse3th <se3thyt@gmail.com>
2026-04-21 08:17:20 -0700

    Add files via upload

---
+++ Password generator script.py
@@ -0,0 +1,15 @@
+import random
+Letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+Numbers = ['0','1','2','3','4','5','6','7','8','9',]
+Symbols = ['!','@','#','$','%','^','&','*',]
+print(random.choice(Letters))
+print(random.choice(Letters))
+print(random.choice(Letters))
+print(random.choice(Numbers))
+print(random.choice(Numbers))
+print(random.choice(Numbers))
+print(random.choice(Symbols))
+print(random.choice(Symbols))
+print(random.choice(Symbols))
+print('Do NOT share this password with anyone!')
+print("Made by Se3thPlayz on Youtube")
